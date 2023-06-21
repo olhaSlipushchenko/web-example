ef6f593d0010e97bd45e64a33c0cf607e0f7016b
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2023-06-21 13:08:19 +0300

    changed links social

--- src/data.js
+++ src/data.js
@@ -10,9 +10,9 @@ export const pageLinks = [
   { id: 4, href: "#tours", text: "tours" },
 ];
 export const socialLinks = [
-  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 1, href: "https://it-it.facebook.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
-  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+  { id: 3, href: "https://it.squarespace.com/", icon: "fab fa-squarespace" },
 ];
 
 export const services = [
